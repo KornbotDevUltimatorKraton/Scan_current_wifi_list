778507cb76058b324a0bb5382d1082f1c1ee4d3e
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-04-15 15:57:20 +0700

    Add files via upload

--- current_wifi.py
+++ current_wifi.py
@@ -1,17 +1,30 @@
+import math 
 import subprocess 
 wifi_list_rssi = {} #Get the rssi signal strange data of the current wifi 
+
+# Get the wifi localization from rssi from the local robot indoor environment 
+def distance_RSSI(Pt,Pl,rssi): 
+      A = Pt-Pl 
+      c_dat = (A-rssi)*0.014336239
+      d = math.pow(10,c_dat) 
+      return d 
 try:
-   output = subprocess.check_output("iwgetid",shell=True) 
-   name_current_wifi = output.decode().split("\n")[0].split("   ")[1].split(" ")[1].split(":")[1]
-   print(name_current_wifi) 
-   # input the current wifi name to find the current rssi signal strange 
-   wifi_strange = subprocess.check_output("iwlist scanning",shell=True)
-   get_rssi = wifi_strange.decode()
-   print(get_rssi.split("\n")[3].split("    ")[5]) 
-   print(get_rssi.split("\n")[4].split("    ")[5])
-   print(get_rssi.split("\n")[6].split("    ")[5]) 
-   wifi_list_rssi[name_current_wifi] = {'wifi_freq':get_rssi.split("\n")[3].split("    ")[5],'rssi_signal':get_rssi.split("\n")[4].split("    ")[5]}
-   print(wifi_list_rssi) 
+       output = subprocess.check_output("iwgetid",shell=True) 
+       name_current_wifi = output.decode().split("\n")[0].split("   ")[1].split(" ")[1].split(":")[1]
+       print(name_current_wifi) 
+       # input the current wifi name to find the current rssi signal strange 
+       wifi_strange = subprocess.check_output("iwlist scanning",shell=True)
+       get_rssi = wifi_strange.decode()
+       print(get_rssi.split("\n")[3].split("    ")[5]) 
+       print(get_rssi.split("\n")[4].split("    ")[5])
+       print(get_rssi.split("\n")[6].split("    ")[5]) 
+       data_local = get_rssi.split("\n")[4].split("    ")[5].split(" ")
+       print(data_local)
+       A_diff = data_local[0].split("=")[1].split("/")
+       rssi_value = data_local[3].split("=")[1]
+       distance_data =   distance_RSSI(int(A_diff[1]),int(A_diff[0]),float(rssi_value))
+       wifi_list_rssi[name_current_wifi] = {'wifi_freq':get_rssi.split("\n")[3].split("    ")[5].split(":")[1],'rssi_signal':data_local[3].split("=")[1],"A":int(A_diff[1]) - int(A_diff[0]),'distance':distance_data}
+       print(wifi_list_rssi)
    
 except:
    print("Current wifi not found you are not connected")
