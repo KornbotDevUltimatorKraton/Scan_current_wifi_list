3f400a749972f0398e1188460bb5d89c6fe23f46
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-04-16 14:54:38 +0700

    Add files via upload

--- current_wifi.py
+++ current_wifi.py
@@ -1,5 +1,9 @@
 import math 
+import requests 
 import subprocess 
+import time
+from itertools import count 
+
 wifi_list_rssi = {} #Get the rssi signal strange data of the current wifi 
 
 # Get the wifi localization from rssi from the local robot indoor environment 
@@ -7,8 +11,10 @@ def distance_RSSI(Pt,Pl,rssi):
       A = Pt-Pl 
       c_dat = (A-rssi)*0.014336239
       d = math.pow(10,c_dat) 
-      return d 
-try:
+      return d
+
+for i in count(0): # running loop in count  
+  try:
        output = subprocess.check_output("iwgetid",shell=True) 
        name_current_wifi = output.decode().split("\n")[0].split("   ")[1].split(" ")[1].split(":")[1]
        print(name_current_wifi) 
@@ -25,7 +31,10 @@ try:
        distance_data =   distance_RSSI(int(A_diff[1]),int(A_diff[0]),float(rssi_value))
        wifi_list_rssi[name_current_wifi] = {'wifi_freq':get_rssi.split("\n")[3].split("    ")[5].split(":")[1],'rssi_signal':data_local[3].split("=")[1],"A":int(A_diff[1]) - int(A_diff[0]),'distance':distance_data}
        print(wifi_list_rssi)
-   
-except:
-   print("Current wifi not found you are not connected")
+       payload_data = {"kornbot380@hotmail.com":wifi_list_rssi}
+       rssi_post_robot = requests.post("https://roboreactor.com/rssi_robot_client",json=payload_data)
+       print(rssi_post_robot.json())
+       time.sleep(0.5)
+  except:
+       print("Current wifi not found you are not connected")
    
